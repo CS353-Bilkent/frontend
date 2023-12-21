f8822b45f27f299dc36d75c84ab78de024227825
Faruk Ulutaş <faruk.ulutas@ug.bilkent.edu.tr>
2023-12-22 00:44:56 +0300

    Update Workshops.js

--- src/pages/Workshops.js
+++ src/pages/Workshops.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   Grid,
   Typography,
@@ -10,144 +11,45 @@ import {
 import Header from "../components/Header";
 import IconButton from "@mui/material/IconButton";
 import SearchIcon from "@mui/icons-material/Search";
+import axiosInstance from "../service/axiosInterceptor";
 
 export default function Workshops() {
-  let artworks = [
-    {
-      title: "What is Art?",
-      artist: "Rene Magritte",
-      description:
-        "This is a workshop that will teach you about the different types of art and how to appreciate them.",
-      image: "/artworks/rene-magritte.webp",
-    },
-    {
-      title: "What is Art?",
-      artist: "Rene Magritte",
-      description:
-        "This is a workshop that will teach you about the different types of art and how to appreciate them.",
-      image: "/artworks/rene-magritte.webp",
-    },
-    {
-      title: "What is Art?",
-      artist: "Rene Magritte",
-      description:
-        "This is a workshop that will teach you about the different types of art and how to appreciate them.",
-      image: "/artworks/rene-magritte.webp",
-    },
-    {
-      title: "What is Art?",
-      artist: "Rene Magritte",
-      description:
-        "This is a workshop that will teach you about the different types of art and how to appreciate them.",
-      image: "/artworks/rene-magritte.webp",
-    },
-    {
-      title: "What is Art?",
-      artist: "Rene Magritte",
-      description:
-        "This is a workshop that will teach you about the different types of art and how to appreciate them.",
-      image: "/artworks/rene-magritte.webp",
-    },
-  ];
+  const [workshops, setWorkshops] = useState([]);
+
+  useEffect(() => {
+    axiosInstance.get("/workshop/all", {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+    .then(response => {
+      setWorkshops(response.data);
+    })
+    .catch(error => console.error("Error fetching workshops:", error));
+  }, []);
 
   return (
     <>
       <Header />
-      <Grid
-        container
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "flex-start",
-          p: 2,
-          paddingTop: "2vh",
-          width: "100vw",
-          height: "93vh",
-        }}
-      >
-        <Grid
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            p: 2,
-            gap: "2vh",
-            paddingTop: "2vh",
-          }}
-        >
-          <Typography
-            sx={{
-              fontFamily: "Segoe UI",
-              fontWeight: 200,
-              fontSize: 36,
-              marginTop: "2vh",
-            }}
-          >
-            Workshops
-          </Typography>
+      <Grid container sx={{ display: "flex", justifyContent: "center", alignItems: "flex-start", p: 2, paddingTop: "2vh", width: "100vw", height: "93vh" }}>
+        <Grid item xs={12} sx={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", p: 2, gap: "2vh", paddingTop: "2vh" }}>
+          <Typography sx={{ fontFamily: "Segoe UI", fontWeight: 200, fontSize: 36, marginTop: "2vh" }}>Workshops</Typography>
           <Grid>
-            <InputBase
-              sx={{
-                ml: 1,
-                flex: 1,
-                backgroundColor: "#F5F5F5",
-                p: "5px",
-                borderRadius: "5px",
-                width: "50vw",
-              }}
-              placeholder="Search Workshops"
-              inputProps={{ "aria-label": "Search artworks" }}
-            />
+            <InputBase sx={{ ml: 1, flex: 1, backgroundColor: "#F5F5F5", p: "5px", borderRadius: "5px", width: "50vw" }} placeholder="Search Workshops" inputProps={{ "aria-label": "Search workshops" }} />
             <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
               <SearchIcon />
             </IconButton>
           </Grid>
         </Grid>
-
-        <Grid
-          container
-          spacing={5}
-          sx={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            p: 3,
-            width: "100vw",
-          }}
-        >
-          {artworks.map((artwork) => (
-            <Grid item>
+        <Grid container spacing={5} sx={{ display: "flex", justifyContent: "center", alignItems: "center", p: 3, width: "100vw" }}>
+          {workshops.map((workshop, index) => (
+            <Grid item key={index} xs={12} sm={6} md={4}>
               <Card sx={{ maxWidth: 400, width: 300 }}>
                 <CardActionArea>
-                  <CardMedia
-                    component="img"
-                    height="200"
-                    image={artwork.image}
-                    alt="artwork"
-                  />
-                  <CardContent
-                    sx={{
-                      height: "35px",
-                      display: "flex",
-                      flexDirection: "column",
-                      justifyContent: "center",
-                      alignItems: "center",
-                      backdropFilter: "blur(10px)",
-                    }}
-                  >
-                    <Typography
-                      gutterBottom
-                      fontSize={"24"}
-                      fontWeight={700}
-                      fontFamily={"Segoe UI"}
-                      component="div"
-                    >
-                      {artwork.title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                      {artwork.artist}
-                    </Typography>
+                  <CardMedia component="img" height="200" image={workshop.image || "/path/to/default/image"} alt={workshop.title} />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">{workshop.title}</Typography>
+                    <Typography variant="body2" color="text.secondary">{workshop.workshopDescription}</Typography>
                   </CardContent>
                 </CardActionArea>
               </Card>
